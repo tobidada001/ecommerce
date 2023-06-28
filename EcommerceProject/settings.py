2ab053e5db8788e3aac205f7a1b25bc68fb7c139
OLUWATOBI <dadaoluwatobijohn@gmail.com>
2023-06-28 10:14:35 +0100

    Made code ready for railway deployment

--- EcommerceProject/settings.py
+++ EcommerceProject/settings.py
@@ -36,6 +36,7 @@ CKEDITOR_UPLOAD_PATH = 'uploads'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -142,6 +143,8 @@ STATICFILES_DIRS = [BASE_DIR/'staticfiles', ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media'
 STATIC_ROOT = BASE_DIR/'static'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
